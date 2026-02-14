7757bf3e841c5446b7db6585bec0bd002515fe59
Thiago-S-Sousa <Thiago_S_Sousa@outlook.com>
2026-02-14 10:15:15 -0300

    feat: pasta Enums e enums DepartamentoEnum e TurnoEnum adicionados

---
+++ Colabora/Enums/DepartamentoEnum.cs
@@ -0,0 +1,11 @@
+﻿namespace Colabora.Enums
+{
+    public enum DepartamentoEnum
+    {
+        RH,
+        Financeiro,
+        Compras,
+        Atendimento,
+        Zeladoria
+    }
+}
---
+++ Colabora/Enums/TurnoEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace Colabora.Enums
+{
+    public enum TurnoEnum
+    {
+        Manhã,
+        Tarde,
+        Noite
+    }
+}
