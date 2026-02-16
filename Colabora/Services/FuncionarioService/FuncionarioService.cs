1b21e3a88ff3ba069860f7ace2b0f03f4d39fa46
Thiago-S-Sousa <Thiago_S_Sousa@outlook.com>
2026-02-16 11:00:41 -0300

    feat: pasta Services e FuncionarioService adicionadas, IFuncionarioInterface implementada a FuncionarioService

---
+++ Colabora/Services/FuncionarioService/FuncionarioService.cs
@@ -0,0 +1,38 @@
+﻿using Colabora.Interfaces;
+using Colabora.Models;
+
+namespace Colabora.Services.FuncionarioService
+{
+    public class FuncionarioService : IFuncionarioInterface
+    {
+        public Task<ServiceResponse<List<FuncionarioModel>>> CreateFuncionario(FuncionarioModel novoFuncionario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<List<FuncionarioModel>>> DeleteFuncionario(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<FuncionarioModel>> GetFuncionarioById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<List<FuncionarioModel>>> GetFuncionarios()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<List<FuncionarioModel>>> InactiveFuncionario(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<List<FuncionarioModel>>> UpdateFuncionario(FuncionarioModel editadoFuncionario)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
