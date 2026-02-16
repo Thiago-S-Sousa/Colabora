e44a8c35eb8166ed07a6540ee2b6d23cc30e02af
Thiago-S-Sousa <Thiago_S_Sousa@outlook.com>
2026-02-16 11:13:40 -0300

    feat: injeção de depedência de ApplicationDbContext em FuncionarioService, estabelecendo conexão com o banco de dados

--- Colabora/Services/FuncionarioService/FuncionarioService.cs
+++ Colabora/Services/FuncionarioService/FuncionarioService.cs
@@ -1,10 +1,17 @@
-﻿using Colabora.Interfaces;
+﻿using Colabora.DataContext;
+using Colabora.Interfaces;
 using Colabora.Models;
 
 namespace Colabora.Services.FuncionarioService
 {
     public class FuncionarioService : IFuncionarioInterface
     {
+        private readonly ApplicationDbContext _context;
+        public FuncionarioService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public Task<ServiceResponse<List<FuncionarioModel>>> CreateFuncionario(FuncionarioModel novoFuncionario)
         {
             throw new NotImplementedException();
