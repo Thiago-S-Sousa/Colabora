4e1c9a6e4a9c1c2fb34e9c3449fff08b27b97e71
Thiago-S-Sousa <Thiago_S_Sousa@outlook.com>
2026-02-21 10:55:48 -0300

    feat: FuncionarioInterface criada e métodos necessários assinados

---
+++ Colabora/Interfaces/FuncionarioInterface/IFuncionarioInterface.cs
@@ -0,0 +1,14 @@
+﻿using Colabora.Models;
+
+namespace Colabora.Interfaces.FuncionarioInterface
+{
+    public interface IFuncionarioInterface
+    {
+        Task<ServiceResponse<List<FuncionarioModel>>> GetFuncionarios();
+        Task<ServiceResponse<FuncionarioModel>> GetFuncionarioById(int id);
+        Task<ServiceResponse<List<FuncionarioModel>>> CreatedFuncionario(FuncionarioModel novoFuncionario);
+        Task<ServiceResponse<List<FuncionarioModel>>> UpdatedFuncionario(FuncionarioModel editadoFuncionario);
+        Task<ServiceResponse<List<FuncionarioModel>>> RemovedFuncionario(int id);
+        Task<ServiceResponse<List<FuncionarioModel>>> InactivedFuncionario(int id);
+    }
+}
